dcc8036b6d31dc140c1497b0a7581e6078e3e80d
Pumpkin Mian <zhiyongwang007@foxmail.com>
2017-12-17 16:59:32 +0800

    ship and bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,32 @@
 #!/user/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
-
 import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
-    # 初始化游戏并创建一个屏幕对象
+    # 初始化pygame、设置和屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((600, 400))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞创
+    ship = Ship(ai_settings, screen)
+
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 开始游戏主循环
     while True:
-
-        # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
